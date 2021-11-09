6c8f79fba09062d0fb104c0f6839fe784e406dae
Nico Biernat <stu209212@mail.uni-kiel.de>
2021-11-09 16:59:27 +0100

    Make websocket url configurable in webinterface

--- static/fileserver.go
+++ static/fileserver.go
@@ -2,7 +2,6 @@ package static
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -13,24 +12,16 @@ import (
 var (
 	webinterfaceRoute = config.GetString("WEBINTERFACE_ROUTE", "/")
 	webinterfacePort  = config.GetInt("WEBINTERFACE_PORT", 3001)
-	websocketPort     = config.GetInt("WEBSOCKET_PORT", 3000)
 )
 
 func StartFileserver() {
 	// serve static testing site (only works with websocket endpoint enabled)
 	log.Println("Serving static files: " + "http://localhost:" + strconv.Itoa(webinterfacePort) + webinterfaceRoute)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/websocket-port", GetWebsocketPort)
 	mux.Handle(webinterfaceRoute, http.FileServer(http.Dir("./static")))
 	serv := &http.Server{
 		Addr:    ":" + fmt.Sprint(webinterfacePort),
 		Handler: mux,
 	}
 	go log.Fatal(serv.ListenAndServe())
-}
-
-// GET route for the webinterface to get the websocket port
-func GetWebsocketPort(w http.ResponseWriter, r *http.Request) {
-	log.Println("Sending websocket port: ", []byte(fmt.Sprint(websocketPort)))
-	io.WriteString(w, fmt.Sprint(websocketPort))
 }
